ec419b768ba3d69875f28ad23e552772254a0b05
Ozakpolor Emmanuel <ozakpolorovis@gmail.com>
2026-05-20 13:37:55 +0100

    fix remaining href in Footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {
   BrowserRouter as Router,
   Routes,
@@ -76,30 +75,4 @@ function App() {
 }
 
 export default App
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
->>>>>>> c5d1999 (Initialize project using Create React App)
+import React from 'react'                                                     
